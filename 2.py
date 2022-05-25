983ac90222854c7648709eacedc150adf493a681
TwilightSparkle334 <74260583+TwilightSparkle334@users.noreply.github.com>
2022-05-25 14:29:45 +0600

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,12 @@
+from multiprocessing import Condition
+
+
+if today == 'Saturday':
+     print ('Party!!')
+elif today == 'Sunday': 
+    if condition == 'Headache':
+        print ('Recover, then rest.')
+    else:
+        print ('Rest.')
+else:
+        print ('Work, Work, Work.')
